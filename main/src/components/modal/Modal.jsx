da5fc04713bab8d3bfd8ca6fb43bb86e64b11a05
rweekscodes <richard.weeks1946@gmail.com>
2024-01-01 02:58:07 +0100

    nc

--- main/src/components/modal/Modal.jsx
+++ main/src/components/modal/Modal.jsx
@@ -4,7 +4,6 @@ import { ig as Instagram } from "./../../assets/svg/ig";
 import { fb as Facebook } from "./../../assets/svg/fb";
 import { cl as Close } from "./../../assets/svg/cl";
 import "./md.css";
-import { useEffect } from "react";
 
 // eslint-disable-next-line react/prop-types
 const Modal = ({ open, setOpen, text, mode }) => {
@@ -14,7 +13,7 @@ const Modal = ({ open, setOpen, text, mode }) => {
 
   open
     ? setTimeout(() => {
-        window.location.href = "https://unity-in-diversity.onrender.com";
+        window.location.href = "https://unity-in-diversiity.onrender.com";
       }, 2000)
     : 0;
 
@@ -44,37 +43,6 @@ const Modal = ({ open, setOpen, text, mode }) => {
             Redirecting you to our voters page. This might take a while. please
             be patient...
           </p>
-          {/* <div className="buttons">
-            <h2>Create my account with</h2>
-            <div className="btns">
-              <a
-                href="https://vote-akei.onrender.com/facebook"
-                target="_self"
-                className="fb"
-              >
-                <Facebook />
-                <p className="btn-txt">Facebook</p>
-              </a>
-              <a
-                href="https://vote-akei.onrender.com/instagram"
-                target="_parent"
-                className="ig"
-              >
-                <Instagram /> <p className="btn-txt">Instagram</p>
-              </a>
-              <div className="sepe">
-                <hr />
-                <sub>Or</sub> <hr />
-              </div>
-              <a
-                className="gl"
-                href="https://vote-akei.onrender.com/google"
-                target="_parent"
-              >
-                <Google /> <p className="btn-txt">Google</p>
-              </a>
-            </div>
-          </div> */}
         </div>
         <p className="lil">
           Join us on this exciting journey of self-expression, empowerment, and
